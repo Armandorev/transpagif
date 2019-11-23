94e42fe25f0ed7bbbad9f67744e755bc229a0654
Armando Sanchez Medina <armandosanchezmedina@gmail.com>
2019-11-23 23:01:05 +0000

    wip

--- transpagif.py
+++ transpagif.py
@@ -14,8 +14,9 @@ for f in folders:
     rgb_image = im.convert('RGBA')
     data = numpy.array(rgb_image)
     red, green, blue, alpha = data.T
-    r, g, b, a = rgb_image.getpixel((1, 1))
+    r, g, b = rgb_image.getpixel((1, 1))
     background_areas = (red == r) & (green == g) & (blue == b)
-    data[..., :-1][background_areas.T] = (r, g, b)
+    data[..., :-1][background_areas.T] = (r, g, b, 0)
     image_transparent = Image.fromarray(data)
+    print(r,g, b, a , "preparing data for image %s " % f.replace('.gif','.png') )
     # image_transparent.save(f.replace('.gif','.png'), "PNG")
