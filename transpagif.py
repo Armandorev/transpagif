92868843e66dcb98335adcbf0668b558076cdddb
Armando Sanchez Medina <armandosanchezmedina@gmail.com>
2019-11-24 08:40:03 +0000

    working version - removes background of pizel at 0,0

--- transpagif.py
+++ transpagif.py
@@ -1,7 +1,7 @@
 import glob
 import sys
 from PIL import Image
-import numpy
+import numpy as np
 
 
 path = str(sys.argv[1])
@@ -12,11 +12,15 @@ folders = [f for f in glob.glob(path + '/**/*.gif',  recursive=True)]
 for f in folders:
     im = Image.open(f)
     rgb_image = im.convert('RGBA')
-    data = numpy.array(rgb_image)
-    red, green, blue, alpha = data.T
-    r, g, b = rgb_image.getpixel((1, 1))
-    background_areas = (red == r) & (green == g) & (blue == b)
-    data[..., :-1][background_areas.T] = (r, g, b, 0)
+    r_pixelMask, g_pixelMask, b_pixelMask, a_pixelMask  = rgb_image.getpixel((1, 1))
+    r_transparent, g_transparent, b_transparent, a_transparent  = r_pixelMask, g_pixelMask, b_pixelMask, 0
+    data = np.array(rgb_image)
+
+    red, green, blue, alpha = data[:,:,0], data[:,:,1], data[:,:,2], data[:,:,3]
+    mask = (red == r_pixelMask) & (green == g_pixelMask) & (blue == b_pixelMask)
+
+    data[:,:,:4][mask] = [r_transparent, g_transparent, b_transparent, a_transparent]
+
     image_transparent = Image.fromarray(data)
-    print(r,g, b, a , "preparing data for image %s " % f.replace('.gif','.png') )
-    # image_transparent.save(f.replace('.gif','.png'), "PNG")
+    print("preparing data for image %s " % f.replace('.gif','.png') )
+    image_transparent.save(f.replace('.gif','.png'), "PNG")
